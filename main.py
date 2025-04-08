bc85405d1cf0fc4dd2085412a69d819e2cdd2156
amirshapkota <shapkotaamir0@gmail.com>
2025-04-08 23:02:39 +0545

    created: main

---
+++ main.py
@@ -0,0 +1,11 @@
+from generator import generate_qr_code
+from utils import get_data_input, get_filename_input
+
+def main():
+    data = get_data_input()
+
+    filename = get_filename_input()
+
+    generate_qr_code(data, filename)
+
+main()
